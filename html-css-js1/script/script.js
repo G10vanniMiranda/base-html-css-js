bde2c85c541bf142806b8a9ba2da73bf85ab8054
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-01-13 23:23:57 -0400

    updated-js

---
+++ html-css-js1/script/script.js
@@ -0,0 +1,8 @@
+var btnCadastrar = document.querySelector('#btnCadastrar')
+var ctx = document.querySelector('.container')
+
+btnCadastrar.onclick = function(event){
+    event.preventDefault()
+
+    alert('Hello World!')
+}

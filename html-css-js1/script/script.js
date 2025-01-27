d1404834adcf64aab0f87c0f23a7dd7bb6003810
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-01-26 23:43:26 -0400

    update

--- html-css-js1/script/script.js
+++ html-css-js1/script/script.js
@@ -1,5 +1,10 @@
+
 var btnCadastrar = document.querySelector('#btnCadastrar')
 var ctx = document.querySelector('.container')
+
+// console.log(btnCadastrar)
+// console.log(btnCadastrar)
+// console.log(ctx)
 
 btnCadastrar.onclick = function(event){
     event.preventDefault()
